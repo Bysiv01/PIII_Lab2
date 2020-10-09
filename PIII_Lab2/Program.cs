f3725c2a051fcdabe78150732547a4d6a0b80595
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2020-10-09 03:11:33 +0200

    init

--- PIII_Lab2/Program.cs
+++ PIII_Lab2/Program.cs
@@ -4,9 +4,39 @@ namespace PIII_Lab2
 {
     class Program
     {
+        enum Wyniki
+        {
+            JednoMZ,
+            DwaMZ,
+            ZeroMZ,
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a;
+            double b;
+            double c;
+            double delta;
+
+            Console.WriteLine("Podaj a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj c");
+            c = Convert.ToDouble(Console.ReadLine());
+            
+            delta = (b * b - 4 * a * c);
+
+            Console.WriteLine($"Delta jest rowna: {delta}");
+
+            Wyniki wynik = Wyniki.JednoMZ | Wyniki.DwaMZ | Wyniki.ZeroMZ;
+
+            switch (wynik)
+            {
+                
+            }
+           
         }
     }
+    
 }
