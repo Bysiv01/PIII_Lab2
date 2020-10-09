51d41ab145bc4b83dd3e6e2d5e53cfffbbce805a
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2020-10-09 23:45:23 +0200

    Update Program.cs

--- PIII_Lab2/Program.cs
+++ PIII_Lab2/Program.cs
@@ -13,52 +13,59 @@ namespace PIII_Lab2
 
         static void Main(string[] args)
         {
-            double a;
-            double b;
-            double c;
-            double delta;
-
-            Console.WriteLine("Podaj a");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Podaj b");
-            b = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Podaj c");
-            c = Convert.ToDouble(Console.ReadLine());
-
-            delta = (b * b - 4 * a * c);
-
-            Console.WriteLine($"Delta jest rowna: {delta}");
-
-            Wyniki wyniki = Wyniki.ZeroMZ;
-            wyniki |= Wyniki.JednoMZ;
-            wyniki |= Wyniki.DwaMZ;
-
-            if (delta == 0)
+            try
             {
-                wyniki = Wyniki.JednoMZ;
+                Console.WriteLine("Podaj A");
+                string a = Console.ReadLine();
+                double aa = double.Parse(a);
+                Console.WriteLine("Podaj B");
+                string b = Console.ReadLine();
+                double bb = double.Parse(b);
+                Console.WriteLine("Podaj C");
+                string c = Console.ReadLine();
+                double cc = double.Parse(c);
+
+                Double delta = (bb * bb - 4 * aa * cc);
+
+                Console.WriteLine($"Delta wynosi: "{ delta});
+
+                Wyniki wyniki = Wyniki.ZeroMZ;
+                wyniki |= Wyniki.JednoMZ;
+                wyniki |= Wyniki.DwaMZ;
+
+                if (delta == 0)
+                {
+                    wyniki = Wyniki.JednoMZ;
+                }
+                else if (delta > 0)
+                {
+                    wyniki = Wyniki.DwaMZ;
+                }
+                else
+                {
+                    wyniki = Wyniki.ZeroMZ;
+                }
+
+
+                switch (wyniki)
+                {
+                    case Wyniki.ZeroMZ:
+                        Console.WriteLine("Jest 0 miejsc zerowych");
+                        break;
+                    case Wyniki.JednoMZ:
+                        Console.WriteLine("Jest 1 miejsce zerowe");
+                        break;
+                    case Wyniki.DwaMZ:
+                        Console.WriteLine("Są 2 miejsca zerowe");
+                        break;
+                }
             }
-            else if (delta > 0)
+            catch (FormatException)
             {
-                wyniki = Wyniki.DwaMZ;
-            }
-            else
-            {
-                wyniki = Wyniki.ZeroMZ;
+                Console.WriteLine("któraś z podanych wartości nie jest liczba!");
             }
 
 
-            switch (wyniki)
-            {
-                case Wyniki.ZeroMZ:
-                    Console.WriteLine("Jest 0 miejsc zerowych");
-                    break;
-                case Wyniki.JednoMZ:
-                    Console.WriteLine("Jest 1 miejsce zerowe");
-                    break;
-                case Wyniki.DwaMZ:
-                    Console.WriteLine("Są 2 miejsca zerowe");
-                    break;
-            }
         }
     }
     
